a585ba74c0ae9e167085951015f757f82c187661
Yuta Hinokuma <higumachan@users.noreply.github.com>
2024-06-13 01:18:49 +0900

    implement to_pydantic_model

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use pyo3::{IntoPy, PyObject, Python};
+use pyo3::{IntoPy, PyObject, Python, ToPyObject};
 use pyo3::types::IntoPyDict;
 use serde::de::DeserializeOwned;
+use serde::Serialize;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -11,6 +12,7 @@ pub enum Error {
     SerdeError(#[from] serde_json::Error),
 }
 
+/// Convert a Pydantic model to a Rust struct
 pub fn from_pydantic_model<T: DeserializeOwned>(
     py: Python,
     pydantic_model: PyObject,
@@ -28,14 +30,43 @@ pub fn from_pydantic_model<T: DeserializeOwned>(
     Ok(serde_json::from_str(&model)?)
 }
 
+/// Convert a Rust struct to a Pydantic model
+pub fn to_pydantic_model<T: Serialize>(
+    py: Python,
+    model: T,
+    python_package_path: &str,
+    model_name: &str,
+) -> Result<PyObject, Error> {
+    let model_json = serde_json::to_string(&model)?;
+    let locals = [
+        (
+            "model_package",
+            py.import_bound(python_package_path)?.into_py(py),
+        ),
+        ("model_json", model_json.to_object(py)),
+    ]
+    .into_py_dict_bound(py);
+    Ok(py
+        .eval_bound(
+            &format!(
+                "model_package.{}.model_validate_json(model_json)",
+                model_name
+            ),
+            None,
+            Some(&locals),
+        )?
+        .to_object(py))
+}
+
 #[cfg(test)]
 mod tests {
+    use pyo3::prelude::{PyAnyMethods, PyTypeMethods};
     use pyo3::ToPyObject;
     use serde::Deserialize;
 
     use super::*;
 
-    #[derive(Deserialize, Debug)]
+    #[derive(Serialize, Deserialize, Debug)]
     struct Pet {
         name: String,
         age: u64,
@@ -47,7 +78,7 @@ mod tests {
         age: u64,
     }
 
-    #[derive(Deserialize, Debug)]
+    #[derive(Serialize, Deserialize, Debug)]
     #[allow(dead_code)]
     struct Plant {
         kind: String,
@@ -90,6 +121,74 @@ mod tests {
             let pet = setup(py);
             let plant: Result<Plant, Error> = from_pydantic_model(py, pet, vec![]);
             assert!(matches!(plant, Err(Error::SerdeError(_))));
+        });
+    }
+
+    #[test]
+    fn test_to_pydantic_model() {
+        Python::with_gil(|py| {
+            let pet = Pet {
+                name: "Garfield".to_string(),
+                age: 42,
+            };
+            let pydantic_pet = to_pydantic_model(py, pet, "pyo3_pydantic.model", "Pet").unwrap();
+            let locals = [("pet", pydantic_pet)].into_py_dict_bound(py);
+            let pet_name = py
+                .eval_bound("pet.name", None, Some(&locals))
+                .unwrap()
+                .to_string();
+            assert_eq!(pet_name, "Garfield");
+            let pet_age = py
+                .eval_bound("pet.age", None, Some(&locals))
+                .unwrap()
+                .extract::<u64>()
+                .unwrap();
+            assert_eq!(pet_age, 42);
+        });
+    }
+
+    #[test]
+    fn test_to_pydantic_model_validation_error() {
+        Python::with_gil(|py| {
+            let plant = Plant {
+                kind: "Tree".to_string(),
+            };
+            let pydantic_pet: Result<PyObject, Error> =
+                to_pydantic_model(py, plant, "pyo3_pydantic.model", "Pet");
+            dbg!(&pydantic_pet);
+            assert!(matches!(pydantic_pet, Err(Error::Pyo3Error(_))));
+            match pydantic_pet {
+                Err(Error::Pyo3Error(err)) => {
+                    let exception_value = err.get_type_bound(py).name().unwrap().to_string();
+                    assert_eq!(
+                        exception_value,
+                        "pydantic_core._pydantic_core.ValidationError"
+                    );
+                }
+                _ => panic!("Expected Pyo3Error"),
+            }
+        });
+    }
+
+    #[test]
+    fn test_to_pydantic_model_not_found_package_error() {
+        Python::with_gil(|py| {
+            let pet = Pet {
+                name: "Garfield".to_string(),
+                age: 42,
+            };
+
+            let pydantic_pet: Result<PyObject, Error> =
+                to_pydantic_model(py, pet, "pyo3_pydantic.not_found_model", "Pet");
+            dbg!(&pydantic_pet);
+            assert!(matches!(pydantic_pet, Err(Error::Pyo3Error(_))));
+            match pydantic_pet {
+                Err(Error::Pyo3Error(err)) => {
+                    let exception_value = err.get_type_bound(py).name().unwrap().to_string();
+                    assert_eq!(exception_value, "ModuleNotFoundError");
+                }
+                _ => panic!("Expected Pyo3Error"),
+            }
         });
     }
 }
